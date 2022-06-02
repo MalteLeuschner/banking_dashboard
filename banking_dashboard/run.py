1ae5ce7f7448c3b1860f1aa45ecb02f7fb9132a4
leuschnm <malteleuschner@gmx.de>
2022-06-03 00:45:55 +0200

    started experiments in jupyter

--- banking_dashboard/run.py
+++ banking_dashboard/run.py
@@ -1,7 +1,8 @@
 import logging
 
+
 def hello_world(name: str) -> None:
-	''' First init example hello_world '''
+	""" First init example hello_world """
 	message = f'Hello {name}!'
 	logging.getLogger(__name__).info(message)
-	return mesage
+	return message
